78fbc5f6695fcefd43b7dd605212d50d4b6df6f1
Rima971 <madhurima.poddar_ftc@external.swiggy.in>
2024-02-06 09:26:21 +0530

    Created Game that can evaluate player decisions, update their scores and play rounds

---
+++ src/main/java/org/game/Game.java
@@ -0,0 +1,32 @@
+package org.game;
+
+import org.game.enums.Move;
+
+public class Game {
+    private Player firstPlayer;
+    private Player secondPlayer;
+    public Game(Player player1, Player player2){
+        this.firstPlayer = player1;
+        this.secondPlayer = player2;
+    }
+
+    private void startRound(){
+        Move firstPlayerDecision = firstPlayer.decide();
+        Move secondPlayerDecision = secondPlayer.decide();
+
+        if (firstPlayerDecision == Move.COOPERATE){
+            firstPlayer.invest();
+            secondPlayer.gain();
+        }
+        if (secondPlayerDecision == Move.COOPERATE){
+            secondPlayer.invest();
+            firstPlayer.gain();
+        }
+    }
+
+    public void start(int rounds){
+        for (int i=0; i<rounds; i++){
+            this.startRound();
+        }
+    }
+}
