14312b0e59576a244dfdf51d3c2791d7120ad65e
Rima971 <madhurima.poddar_ftc@external.swiggy.in>
2024-02-06 09:22:04 +0530

    Test player for being able to gain or invest

---
+++ src/test/java/PlayerTest.java
@@ -0,0 +1,18 @@
+import org.game.Player;
+import org.game.Score;
+import org.game.playerTypes.Cheater;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTest {
+    @Test
+    public void ableToGainOrInvestCoins(){
+        Player cheater = new Cheater();
+        cheater.gain();
+        assertEquals(new Score(3), cheater.getScores());
+
+        cheater.invest();
+        assertEquals(new Score(2), cheater.getScores());
+    }
+}
